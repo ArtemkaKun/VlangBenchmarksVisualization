4f833e6b39d58c01ca8d507cb7c3c946be2c70ec
Artem Yurchenko <zumikq78@gmail.com>
2021-12-25 11:31:28 +0100

    Parsing table with benchmarks and parsing table headers was implemented

--- vlang_page_parser/parser.v
+++ vlang_page_parser/parser.v
@@ -1,5 +1,19 @@
 module vlang_page_parser
 
+import net.html
+
 pub fn parse_html(html_string string) string {
-	return html_string
+	doc := html.parse(html_string)
+	table_rows := doc.get_tag('tr')
+
+	table_headers := table_rows[0].children
+	mut headers := []string{len: table_headers.len, cap: table_headers.len}
+
+	for header_tag_index, header_tag in table_headers {
+		headers[header_tag_index] = header_tag.content
+	}
+
+	println(headers)
+
+	return ''
 }
