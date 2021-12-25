e087e3a1c14f09d307e5599e8b72bc118a524108
Artem Yurchenko <zumikq78@gmail.com>
2021-12-25 10:44:34 +0100

    IVlangBenchmarkRecord interface was removed; VlangBenchmarkData struct became pub

--- data_structures/benchmark_data_structure.v
+++ data_structures/benchmark_data_structure.v
@@ -2,22 +2,11 @@ module data_structures
 
 import time
 
-pub interface IVlangBenchmarkRecord {
-	set_result(value string)
-	get_benchmark_name() string
-	get_date() time.Time
-	get_commit_hash() string
-	get_commit_message() string
-	get_numerical_result() int
-	get_measure_unit() string
-}
-
-struct VlangBenchmarkData {
-	benchmark_name string
-	date           time.Time
-	commit_hash    string
-	commit_message string
-mut:
+pub struct VlangBenchmarkData {
+	benchmark_name   string
+	date             time.Time
+	commit_hash      string
+	commit_message   string
 	numerical_result int
 	measure_unit     string
 }
