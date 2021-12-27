f4f8f8381d0b43966c4de6616e9d80bc78f8793a
Artem Yurchenko <zumikq78@gmail.com>
2021-12-27 21:17:19 +0100

    main.v - code was reimplemented to behave like server side app

--- main.v
+++ main.v
@@ -1,10 +1,23 @@
 import data_collector as data
 import data_processor as processor
+import vweb
+
+struct App {
+	vweb.Context
+}
 
 fn main() {
+	mut app := &App{}
+	app.handle_static('assets', true)
+	app.serve_static('/v-logo.png', 'assets/images/v-logo.png')
+	vweb.run(app, 8080)
+}
+
+pub fn (mut app App) index() vweb.Result {
 	benchmarks := data.get_benchmarks_data('https://fast.vlang.io/')
 	grouped_benchmarks_by_test := processor.get_grouped_benchmarks_data(benchmarks)
 	average_result_by_test := processor.calculate_average_result(grouped_benchmarks_by_test)
+	formated_average_result := processor.format_average_result(average_result_by_test)
 	median_result_by_test := processor.calculate_median_result(grouped_benchmarks_by_test)
-	println(median_result_by_test)
+	return $vweb.html()
 }
