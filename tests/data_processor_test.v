04c4fb21ecb4dbfa26edae034c911d718ebc19da
Artem Yurchenko <zumikq78@gmail.com>
2022-01-02 19:47:59 +0100

    test_calculate_median_result_method test was implemented and passed

--- tests/data_processor_test.v
+++ tests/data_processor_test.v
@@ -49,3 +49,49 @@ fn test_calculate_average_result_method() {
 	average_result := proccessor.calculate_average_result(test_cases)
 	assert math.abs(average_result['test'] - 5.41666667) < 0.0001
 }
+
+fn test_calculate_median_result_method() {
+	test_cases := {
+		'test': [
+			structures.VlangBenchmarkData{
+				numerical_result: 1
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 2
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 3
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 4
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 5
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 5
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 5
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 6
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 7
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 8
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 9
+			},
+			structures.VlangBenchmarkData{
+				numerical_result: 10
+			},
+		]
+	}
+
+	median_result := proccessor.calculate_median_result(test_cases)
+	assert median_result['test'] == 5
+}
